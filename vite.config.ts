66f26948d2f2fd04c828d13eec1c0a900c716790
André Jordão <andre.cjordao@gmail.com>
2025-11-07 23:49:42 +0000

    Add dompurify dependency and update .gitignore for IDE files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['dompurify']
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
